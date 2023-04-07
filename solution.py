de57b662d620b56711bc20e0e68cc78ece6b01b9
ksusha2002 <102557516+ksusha2002@users.noreply.github.com>
2023-04-07 22:16:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
-
-from scipy.stats import norm
-
+from scipy.stats import t
 chat_id = 506081330
-
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    lower_quantile = np.percentile(x, alpha/2 * 100)
-    upper_quantile = np.percentile(x, (1 - alpha/2) * 100)
-    return 0.071, upper_quantile
+  n = len(x)
+  alpha = 1 - p
+  x_mean = np.mean(x)
+  s = np.sqrt(np.var(x, ddof=1))
+  b_hat = x_mean + (x_mean - 0.071) / (n - 1)
+  t_alpha_2 = t.ppf(1 - alpha / 2, n - 1)
+  left = b_hat - t_alpha_2 * s / np.sqrt(n - 1)
+  right = b_hat + t_alpha_2 * s / np.sqrt(n - 1)
+  return (left, right)
 
